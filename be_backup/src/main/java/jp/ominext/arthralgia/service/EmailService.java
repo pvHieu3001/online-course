fdac30c7c43a3f79b4e99c667ea83f95fd53ffd1
dbh92 <63711226+dbh92@users.noreply.github.com>
2025-06-04 23:10:47 +0700

    Delete be_backup directory

--- be_backup/src/main/java/jp/ominext/arthralgia/service/EmailService.java
+++
@@ -1,6 +0,0 @@
-package jp.ominext.arthralgia.service;
-
-public interface EmailService {
-    void sendText(String from, String to, String subject, String body);
-    void sendHTML(String from, String to, String subject, String body);
-}
