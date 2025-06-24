c4c97b0c169ade7e5bdcc63137b85439f65b72e7
hieupv14 <phamvanhieu300198@gmail.com>
2025-06-24 23:20:29 +0700

    Update home page

--- fe/src/page/[role]/(base)/PageHome/index.tsx
+++ fe/src/page/[role]/(base)/PageHome/index.tsx
@@ -16,7 +16,7 @@ function PageHome() {
       <div className={styles.feature}>
         <div className={styles.featureWrapper}>
           <img
-            className={styles.featureIcon}
+            className={styles.featureIconRt1}
             data-bb-lazy='true'
             loading='lazy'
             src='https://khoahocgiasieure.com/storage/rt1.png'
@@ -27,58 +27,43 @@ function PageHome() {
             <p>Hoàn tiền nếu như khoá học không như mô tả</p>
           </div>
         </div>
-        <div className='col-xl-3 col-lg-6 col-md-6 col-sm-6'>
-          <div className='tp-feature-item d-flex align-items-center'>
-            <div className='tp-feature-icon mr-15'>
-              <span>
-                <img
-                  data-bb-lazy='true'
-                  loading='lazy'
-                  src='https://khoahocgiasieure.com/storage/time.png'
-                  alt='Kích hoạt nhanh chóng'
-                />
-              </span>
-            </div>
-            <div className='tp-feature-content'>
-              <h3 className='tp-feature-title'>Kích hoạt nhanh chóng</h3>
-              <p>Nhận khoá học qua email trong vòng 30s</p>
-            </div>
+        <div className={styles.featureWrapper}>
+          <img
+            className={styles.featureIconTime}
+            data-bb-lazy='true'
+            loading='lazy'
+            src='https://khoahocgiasieure.com/storage/time.png'
+            alt='Kích hoạt nhanh chóng'
+          />
+          <div className={styles.featureContent}>
+            <h3 className={styles.featureTitle}>Kích hoạt nhanh chóng</h3>
+            <p>Nhận khoá học qua email trong vòng 30s</p>
           </div>
         </div>
-        <div className='col-xl-3 col-lg-6 col-md-6 col-sm-6'>
-          <div className='tp-feature-item d-flex align-items-center'>
-            <div className='tp-feature-icon mr-15'>
-              <span>
-                <img
-                  data-bb-lazy='true'
-                  loading='lazy'
-                  src='https://khoahocgiasieure.com/storage/rt3.png'
-                  alt='Mua hàng tiết kiệm'
-                />
-              </span>
-            </div>
-            <div className='tp-feature-content'>
-              <h3 className='tp-feature-title'>Mua hàng tiết kiệm</h3>
-              <p>Khóa học với giá rẻ nhất thị trường hiện nay</p>
-            </div>
+        <div className={styles.featureWrapper}>
+          <img
+          className={styles.featureIconRt1}
+            data-bb-lazy='true'
+            loading='lazy'
+            src='https://khoahocgiasieure.com/storage/rt3.png'
+            alt='Mua hàng tiết kiệm'
+          />
+          <div className={styles.featureContent}>
+            <h3 className={styles.featureTitle}>Mua hàng tiết kiệm</h3>
+            <p>Khóa học với giá rẻ nhất thị trường hiện nay</p>
           </div>
         </div>
-        <div className='col-xl-3 col-lg-6 col-md-6 col-sm-6'>
-          <div className='tp-feature-item d-flex align-items-center'>
-            <div className='tp-feature-icon mr-15'>
-              <span>
-                <img
-                  data-bb-lazy='true'
-                  loading='lazy'
-                  src='https://khoahocgiasieure.com/storage/rt4.png'
-                  alt='Hỗ trợ trực tuyến'
-                />
-              </span>
-            </div>
-            <div className='tp-feature-content'>
-              <h3 className='tp-feature-title'>Hỗ trợ trực tuyến</h3>
-              <p>Liên hệ fanpage để được hỗ trợ ngay </p>
-            </div>
+        <div className={styles.featureWrapper}>
+          <img
+          className={styles.featureIconRt1}
+            data-bb-lazy='true'
+            loading='lazy'
+            src='https://khoahocgiasieure.com/storage/rt4.png'
+            alt='Hỗ trợ trực tuyến'
+          />
+          <div className={styles.featureContent}>
+            <h3 className={styles.featureTitle}>Hỗ trợ trực tuyến</h3>
+            <p>Liên hệ fanpage để được hỗ trợ ngay </p>
           </div>
         </div>
       </div>
