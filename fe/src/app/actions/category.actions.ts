5e03cc581e87c39cb41d1e43eba2a4e0728626a9
hieupv14 <phamvanhieu300198@gmail.com>
2025-07-17 23:32:21 +0700

    gg

--- fe/src/app/actions/category.actions.ts
+++ fe/src/app/actions/category.actions.ts
@@ -1,7 +1,7 @@
 import { Dispatch } from '@reduxjs/toolkit'
 import { categoryConstants } from '../../constants'
 import { categoryServices } from '../services'
-import { fetchedDone, isFetching } from '../slices/category.reducer'
+import { fetchedDone, getCategoriesSuccessFully, isFetching } from '../slices/category.reducer'
 
 export const getCategories = (data) => async (dispatch: Dispatch) => {
   dispatch(isFetching())
