44e97431dfe50aa5ad10cd6e26ad0c7dff51bbe1
hieupv14 <phamvanhieu300198@gmail.com>
2025-08-03 11:34:11 +0700

    gg

--- fe/vite.config.ts
+++ fe/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [{ find: '@', replacement: '/src' }]
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }]
   }
 })
