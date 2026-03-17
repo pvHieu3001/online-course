e76de12f65a8a57186d2009021a87a05f1ea00ca
hieupv14 <phamvanhieu300198@gmail.com>
2026-03-17 23:07:30 +0700

    gg

--- be/src/main/java/online/course/market/scheduler/ThreadScheduler.java
+++ be/src/main/java/online/course/market/scheduler/ThreadScheduler.java
@@ -5,7 +5,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import online.course.market.entity.dto.thread.ThreadAccount;
 import online.course.market.entity.model.PostEntity;
+import online.course.market.entity.model.UserModel;
 import online.course.market.repository.PostRepository;
+import online.course.market.repository.UserRepository;
 import online.course.market.service.GroqService;
 import online.course.market.service.ThreadsService;
 import org.springframework.beans.factory.annotation.Value;
@@ -46,51 +48,44 @@ public class ThreadScheduler {
 
     private final ThreadsService threadsService;
     private final PostRepository postRepository;
-    private List<ThreadAccount> accounts;
+    private final UserRepository userRepository;
 
-    @PostConstruct
-    public void init() {
-        accounts = List.of(
-                new ThreadAccount(account1, accountId1),
-                new ThreadAccount(account2, accountId2),
-                new ThreadAccount(account3, accountId3),
-                new ThreadAccount(account4, accountId4),
-                new ThreadAccount(account5, accountId5)
-        );
-    }
-
-
-
-    @Scheduled(cron = "0 0 6,9,12,15,18,21 * * *", zone = "Asia/Ho_Chi_Minh")
-    //    @Scheduled(cron = "0 * * * * *")
+    // @Scheduled(cron = "0 0 0,6,9,12,15,18,21 * * *", zone = "Asia/Ho_Chi_Minh")
+       @Scheduled(cron = "0 * * * * *")
     public void runMultiAccountPost() {
-        log.info("Bắt đầu tiến trình đăng bài phân tách thời gian: " + LocalDateTime.now());
+        log.info("Bắt đầu tiến trình đăng bài phân tách thời gian: {}", LocalDateTime.now());
 
-        for (int i = 0; i < accounts.size(); i++) {
-            ThreadAccount account = accounts.get(i);
-            if (StringUtils.isEmpty(account.getId())) continue;
+        List<UserModel> users = userRepository.findAll();
+
+        for (int i = 0; i < users.size(); i++) {
+            UserModel account = users.get(i);
+
+            if (account == null || !StringUtils.hasText(account.getThreadId()) || !StringUtils.hasText(account.getThreadToken())) {
+                log.warn("Bỏ qua account do thiếu thông tin định danh hoặc token.");
+                continue;
+            }
 
             int delayInMinutes = i * 15;
-
             CompletableFuture.runAsync(() -> {
                 try {
                     if (delayInMinutes > 0) {
                         log.info("Account {} sẽ đợi {} phút trước khi đăng...", account.getId(), delayInMinutes);
-                        Thread.sleep(delayInMinutes * 60 * 1000L);
+                        Thread.sleep((long) delayInMinutes * 60 * 1000);
                     }
-
                     processSingleAccountPost(account);
-
                 } catch (InterruptedException e) {
+                    log.error("Tiến trình đợi của account {} bị ngắt quãng", account.getId());
                     Thread.currentThread().interrupt();
+                } catch (Exception e) {
+                    log.error("Lỗi không xác định khi xử lý account {}: {}", account.getId(), e.getMessage());
                 }
             });
         }
     }
 
     @Transactional
-    public void processSingleAccountPost(ThreadAccount account) {
-        Optional<PostEntity> postOpt = postRepository.findFirstByIsPublishedFalseAndThreadIdOrderByIdAsc(account.getId());
+    public void processSingleAccountPost(UserModel account) {
+        Optional<PostEntity> postOpt = postRepository.findFirstByIsPublishedFalseAndThreadIdOrderByIdAsc(account.getThreadId());
 
         if (postOpt.isPresent()) {
             PostEntity post = postOpt.get();
@@ -98,7 +93,7 @@ public class ThreadScheduler {
                 String videoUrl = post.getMedias().get(0).getCloudinaryUrl();
                 String imageUrl = (post.getMedias().size() >= 2) ? post.getMedias().get(1).getCloudinaryUrl() : null;
 
-                threadsService.postToThreads(post.getCaption(), imageUrl, videoUrl, post.getAmzUrl(), post, account.getToken(), account.getId());
+                threadsService.postToThreads(post.getCaption(), imageUrl, videoUrl, post.getAmzUrl(), post, account.getThreadToken(), account.getThreadId());
                 log.info("Account {} đã đăng bài ID {} thành công sau thời gian chờ.", account.getId(), post.getId());
 
             } catch (Exception e) {
