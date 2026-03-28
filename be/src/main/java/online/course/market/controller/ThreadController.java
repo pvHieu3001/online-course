f530cbc270be2b137c19fe41265e1352eba78217
hieupv14 <phamvanhieu300198@gmail.com>
2026-03-28 14:22:56 +0700

    gg

--- be/src/main/java/online/course/market/controller/ThreadController.java
+++ be/src/main/java/online/course/market/controller/ThreadController.java
@@ -36,6 +36,7 @@ public class ThreadController {
                 post.setCaption(service.reCreateCap(post.getCaption()));
                 post.setAmzUrl(service.resolveAmazonLink(post.getAmzUrl()));
                 service.downloadAndUpload(post, threadId);
+                Thread.sleep(4000);
             }
             return ResponseEntity.ok("Đã nhận " + posts.size() + " bài viết cho Thread: " + threadId);
         } catch (Exception e) {
